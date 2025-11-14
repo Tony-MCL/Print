374e9ac51287268e7750f41c561aec1618042e72
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-14 22:26:56 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  // Viktig for GitHub Pages – prosjektet ligger på /Print
+  basePath: isProd ? "/Print" : "",
+  assetPrefix: isProd ? "/Print/" : ""
 };
 
 export default nextConfig;
