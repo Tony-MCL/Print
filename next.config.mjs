16b00a8864677cdeeb0b5a6079281bfc79445615
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-14 22:07:04 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-// NB: repo-navn
-const repoName = "Print";
-
-const isProd = process.env.NODE_ENV === "production";
-
-const basePath = isProd ? `/${repoName}` : "";
-
 const nextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
+  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-
-  // Gjør basePath tilgjengelig i kode
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath
-  },
-
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
   }
 };
 
